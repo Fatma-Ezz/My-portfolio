d000e30dc536cde72f37de17951a10c715bce6a2
Fatma Ahmed <fatmaahmedezz@gmail.com>
2025-02-14 10:26:09 -0500

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Fatma Ezz</h1>
+      <p>Frontend Developer</p>
+      <blockquote>
+        "I left my career as an engineer to become a frontend developer and never looked back."
+      </blockquote>
+    </header>
+  );
+}
+
+export default Header;
