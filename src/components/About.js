f4457d8a543d8f8b80b85ef83ef1e59545356ea8
Fatma Ahmed <fatmaahmedezz@gmail.com>
2025-02-14 10:27:05 -0500

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>Hi! I'm Fatma, a frontend developer. You might be wondering, "How does one make that leap?" Well, while perfecting the art of emulsification, I discovered that my true passion lies in the world of code. It turns out, both food engineering and programming share a common ingredient: the thrill of creating something delightful from scratch. Now, instead of whipping up culinary wonders, I'm cooking up clean, efficient code and deliciously interactive websites.</p>
+    </section>
+  );
+}
+
+export default About;
